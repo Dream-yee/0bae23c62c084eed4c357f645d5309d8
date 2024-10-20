a1f886aa225a6ff42dbcdc4a6ab890598d5c7155
yushiuan9499 <yushiuan9499@gmail.com>
2024-10-20 18:25:41 +0800

    新增README.md和一個繼承erc4626的幣

---
+++ FSCS.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC4626.sol";
+contract FSCS is ERC4626{
+
+}
